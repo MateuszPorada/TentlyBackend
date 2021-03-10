c33a96ebcea481f9c9e2743c86ade57ea9c5fdc2
AsonX <mateusz6797@interia.pl>
2021-03-10 16:18:48 +0100

    Fixing swagger config

--- src/main/java/app/tently/tentlyappbackend/configs/MVCConfig.java
+++ src/main/java/app/tently/tentlyappbackend/configs/MVCConfig.java
@@ -3,16 +3,28 @@ package app.tently.tentlyappbackend.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableWebMvc
-public class MVCConfig extends WebMvcConfigurerAdapter {
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
     @Override
     public void configureContentNegotiation(
